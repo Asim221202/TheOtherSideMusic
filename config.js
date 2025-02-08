e85082aa78e46f4e341fbec24268244fc041ddc6
Asim221202 <rahimasim15@gmail.com>
2025-02-08 22:48:34 +0400

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ module.exports = {
   nodes: [
     
     {
-  "name": "ChalresNaig Node",
-  "password": "NAIGLAVA-dash.techbyte.host",
-  "host": "lavahatry4.techbyte.host",
-  "port": 3000,
+  "identifier": "Muzykant v4",
+  "password": "https://discord.gg/v6sdrD9kPh",
+  "host": "lavalink_v4.muzykant.xyz",
+  "port": 80,
   "secure": false
 },
     { 
