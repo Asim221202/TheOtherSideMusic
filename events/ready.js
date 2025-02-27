f4ed458dc603ad6cfc47d100c5ee2a0a4f40aea9
Asim221202 <rahimasim15@gmail.com>
2025-02-27 18:33:35 +0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -24,7 +24,7 @@ module.exports = async (client) => {
 
     async function updateStatus() {
  
-        const activePlayers = Array.from(client.riffy.players.values()).filter(player => player.playing);
+        const activePlayers = Array.from(client.riffy.players.values()).filter(player => player.listening);
 
         if (!activePlayers.length) {
             //console.log("⏹️ No song is currently playing. Setting default status.");
