76bd4f513b91ec69800e11cc00f767c72082dd67
Asim221202 <rahimasim15@gmail.com>
2025-02-27 19:31:14 +0400

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "Şebnem Ferah", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/7qerY7B",
   embedTimeout: 5, 
   errorLog: "", 
